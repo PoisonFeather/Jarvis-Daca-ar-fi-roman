d799757a08028d29fc3be4654deacb7950fa306f
Andrei Biro <andreibiro2003@gmail.com>
2021-03-11 22:29:00 +0200

    ye

--- alege_un_nume.cpp
+++ alege_un_nume.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 
 int main(){
-    char letters[] = "abcdefghijklmnopqrstuvwxyz";
+    char letters[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+                          'h', 'i', 'j', 'k', 'l', 'm', 'n',  
+                          'o', 'p', 'q', 'r', 's', 't', 'u', 
+                          'v', 'w', 'x', 'y', 'z' }; 
     int stop=0;
     while(stop<=10){
         
